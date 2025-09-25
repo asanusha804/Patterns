e00eaede763d7f795f3c59639f4b8714e29d1c6e
Anusha A S <asanusha804@gmail.com>
2025-09-25 06:39:31 +0530

    Update Halfpyramidwithnumbers.java

--- Halfpyramidwithnumbers.java
+++ Halfpyramidwithnumbers.java
@@ -1,1 +1,12 @@
-
+class Patterns {
+  public static void main(String args[]) {
+    int n = 5;
+    for(int i=1; i<=n; i++) {
+      for(int j=1; i<=i; j++) {
+        System.out.print("j");
+      }
+      System.out.println();
+    }
+  }
+}
+        
